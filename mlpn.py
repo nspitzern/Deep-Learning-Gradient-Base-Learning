9a59b231b90511cd99dc5879e19354a389be6465
Nadav Spitzer <nspitzern@gmail.com>
2021-03-31 12:58:04 +0300

    adding start of MLPn

--- mlpn.py
+++ mlpn.py
@@ -1,14 +1,50 @@
 import numpy as np
 
-STUDENT={'name': 'YOUR NAME',
-         'ID': 'YOUR ID NUMBER'}
+STUDENT={'name': 'Nadav Spitzer',
+         'ID': '302228275'}
+
+
+def softmax(x):
+    """
+    Compute the softmax vector.
+    x: a n-dim vector (numpy array)
+    returns: an n-dim vector (numpy array) of softmax values
+    """
+    # YOUR CODE HERE
+    # Your code should be fast, so use a vectorized implementation using numpy,
+    # don't use any loops.
+    # With a vectorized implementation, the code should be no more than 2 lines.
+    x = np.exp(x - np.max(x))
+    x /= np.sum(x)
+    # For numeric stability, use the identify you proved in Ex 2 Q1.
+    return x
+
 
 def classifier_output(x, params):
     # YOUR CODE HERE.
+    # params = list(reversed(params))
+
+    result = x
+
+    for i in range(0, len(params) - 1, 2):
+        mat = params[i]
+        vec = params[i + 1]
+
+        result = np.dot(result, mat) + vec
+
+        # apply tanh to all layers except the last one
+        if i < len(params) - 2:
+            result = np.tanh(result)
+
+    # apply softmax to the last layer
+    probs = softmax(result)
+
     return probs
+
 
 def predict(x, params):
     return np.argmax(classifier_output(x, params))
+
 
 def loss_and_gradients(x, y, params):
     """
@@ -28,7 +64,11 @@ def loss_and_gradients(x, y, params):
     you should not have gW2 and gb2.)
     """
     # YOU CODE HERE
-    return ...
+    y_hat = classifier_output(x, params)
+    loss = -np.log(y_hat[y])
+
+    return loss, None
+
 
 def create_classifier(dims):
     """
@@ -51,5 +91,26 @@ def create_classifier(dims):
     second layer, and so on.
     """
     params = []
+
+    for i in range(0, len(dims) - 1):
+        in_dim = dims[i]
+        out_dim = dims[i + 1]
+
+        mat = xavier_init(in_dim, out_dim)
+        vec = xavier_init(out_dim)
+
+        params.extend([mat, vec])
+
     return params
+
+
+def xavier_init(in_dim, out_dim=None):
+    if out_dim is not None:
+        eps = (np.sqrt(6) / np.sqrt(in_dim + out_dim))
+        param = np.random.uniform(-eps, eps, (in_dim, out_dim))
+    else:
+        eps = (np.sqrt(6) / np.sqrt(in_dim + 1))
+        param = np.random.uniform(-eps, eps, (in_dim, ))
+
+    return param
 
